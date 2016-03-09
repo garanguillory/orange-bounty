b27508a2bc1259c6ad29fb79474710317d8a59f9
Garan Guillory <garanguillory@gmail.com>
2016-03-08 20:42:03 -0700

    Products places aligned on homepage

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+// need to render all fruits from database
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 module.exports = router;
