4f92c983ed25165607c081e6f9bc67231e9382f0
Garan Guillory <garanguillory@gmail.com>
2016-03-10 08:20:41 -0700

    Add knex.js, knexfile.js, and necessary dependencies

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var knex = require('../db/knex');
+function Citrus() {
+  return knex('');
+}
 
 
 // need to render all fruits from database
+// GET all fruits
 router.get('/', function(req, res, next) {
   res.render('index');
 });
@@ -17,5 +21,7 @@ router.get('/products', function(req, res, next) {
 router.get('/checkout', function(req, res, next) {
   res.render('checkout');
 });
+
+
 
 module.exports = router;
