76d31743af415a380cbca4c11c227dafb6d79cd5
Garan Guillory <garanguillory@gmail.com>
2016-03-10 11:56:11 -0700

    edit routes, css, index, and products pages

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('../db/knex');
+var knex = require('../../../db/knex');
 function Fruits() {
   return knex('fruits');
 }
@@ -9,21 +9,31 @@ function Fruits() {
 // need to render all fruits from database
 // GET all fruits
 router.get('/', function(req, res, next) {
-  Fruits().select().then(function(result){
-  	res.render('index', {fruits: fruits} );
+  Fruits().select().limit(3).then(function(result){
+  	res.render('index', {fruits: result} );
   });
 });
 
 
 
 router.get('/products', function(req, res, next) {
-  res.render('products');
+
+		var column = 'rating';
+		var order = 'ASC';
+
+		if(Object.keys(req.query)[0]){
+			column = Object.keys(req.query)[0];
+			order = req.query[column];
+		}
+
+    Fruits().select().orderBy(`${column}`,`${order}`).then(function(result){
+  	res.render('products', {fruits: result} );
+  });
 });
 
 router.get('/checkout', function(req, res, next) {
   res.render('checkout');
 });
-
 
 
 module.exports = router;
