e3489017ad0f88f46e3be180101c0985a5fe53f2
Tyler Rozboril <ty.rozboril@gmail.com>
2016-03-10 10:56:11 -0700

    migration and seeds

--- seeds/fruits.js
+++ seeds/fruits.js
@@ -9,7 +9,7 @@ exports.seed = function(knex, Promise) {
     	 name: 'Orange',
     	 price: 4.23,
     	 rating: 5,
-    	 description: 'Deliciousness',
+    	 description: 'Orange flavored like something that is fanta. Sweet after taste, with a tasty vitimin c kick. "Squish," the onomatopoeia of delectable deliciousness which is an orange.',
          image: 'https://upload.wikimedia.org/wikipedia/commons/7/7b/Orange-Whole-%26-Split.jpg',
     	 outofstock: false
     	}),
@@ -18,7 +18,7 @@ exports.seed = function(knex, Promise) {
          name: 'Tangerine',
          price: 8.23,
          rating: 5,
-         description: 'Deliciousness',
+         description: 'Tangy like something that is pretty tangy. Almost tastes like an orange. Sweet after taste, with a sweet intensity. "Squish," the onomatopoeia of delectable deliciousness which is this Tangerine. Live the dream, dream the Tang.',
          image: 'https://upload.wikimedia.org/wikipedia/commons/7/7b/Orange-Whole-%26-Split.jpg',
          outofstock: false
         }),
@@ -27,7 +27,7 @@ exports.seed = function(knex, Promise) {
          name: 'Manderine',
          price: 0.23,
          rating: 5,
-         description: 'Deliciousness',
+         description: 'Sometimes you really have to think, why the hell are there so many different types of oranges... they all taste fairly similar. But then you remember, you never complained about how many types of apples there are... so SHUT the BONKERS up!',
          image: 'https://upload.wikimedia.org/wikipedia/commons/7/7b/Orange-Whole-%26-Split.jpg',
          outofstock: false
         }),
@@ -36,7 +36,7 @@ exports.seed = function(knex, Promise) {
          name: 'Satsuma',
          price: 1.01,
          rating: 5,
-         description: 'Deliciousness',
+         description: 'Sweet like something that is sweet. Sweet after taste, with immediate sour intensity. "Squish," the onomatopoeia of delectable deliciousness which is this Satsuma.',
          image: 'https://upload.wikimedia.org/wikipedia/commons/7/7b/Orange-Whole-%26-Split.jpg',
          outofstock: false
         }),
@@ -45,7 +45,7 @@ exports.seed = function(knex, Promise) {
          name: 'Grapefruit',
          price: 4.23,
          rating: 5,
-         description: 'Deliciousness',
+         description: 'Tart like something that is kind of tart. Sweet after taste, with a slow, sour-ish, intensity. "Squish," the onomatopoeia of delectable deliciousness which is this Grapefruit.',
          image: 'https://upload.wikimedia.org/wikipedia/commons/f/fe/Grapefruit-Whole-%26-Split.jpg',
          outofstock: false
         }),
@@ -54,7 +54,7 @@ exports.seed = function(knex, Promise) {
          name: 'Lemon',
          price: 0.99,
          rating: 2,
-         description: 'Deliciousness',
+         description: 'Tart like something that is tart. Sweet after taste, with immediate sour intensity. "Squish," the onomatopoeia of delectable deliciousness which is the lemon.',
          image: 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Lemon-Whole-Split.jpg',
          outofstock: false
         }),
@@ -63,16 +63,16 @@ exports.seed = function(knex, Promise) {
          name: 'Lime',
          price: 4.23,
          rating: 2,
-         description: 'Deliciousness',
+         description: 'Limey like something that is lime flavored. Bitter after taste, with immediate intensity of sourness. "Squish," the onomatopoeia of delectable deliciousness which is the Lime.',
          image: 'https://upload.wikimedia.org/wikipedia/commons/6/68/Lime-Whole-Split.jpg',
          outofstock: false
         }),
     knex('fruits').insert(
         {
-         name: 'Satsuma',
-         price: 0.99,
-         rating: 4,
-         description: 'Deliciousness',
+         name: 'Clementine',
+         price: 0.02,
+         rating: 1,
+         description: 'Sweet like something that is not tart. Light after taste, with immediate suculant intensity. "Squish," the onomatopoeia of delectable deliciousness which is NOT this Clementine, it sucks!',
          image: 'https://upload.wikimedia.org/wikipedia/commons/3/31/Citrus_unshiu_20101127_c.jpg',
          outofstock: false
         })
